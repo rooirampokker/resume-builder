a5d29107973e20e613b55899a853e56ff3cf1990
rooirampokker <rooirampokker@gmail.com>
2019-03-19 11:25:55 +0200

    Read Resume JSON and output different sections into Bootstrap tabs

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import FormContainer from './containers/FormContainer';
+import TabContainer from './containers/TabContainer';
+import Resume from './data/Resume.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
+        const Tabs = Object.keys(Resume).map((d, key) => {
+          if (key != 0) {
+              return (
+                  <TabContainer
+                      active=""
+                      tabId={d}
+                      tabLable={d}
+                      key={d}
+                  />
+              )
+          }
+        });
+
         return (
-            <div className='grid-container'>
-            <div className='grid-x grid-padding-x card'>
-            <fieldset className='fieldset'>
-            <legend>Select Excel file for processing</legend>
-                                          <FormContainer />
-                                          </fieldset>
-                                          </div>
-                                          </div>
+            <React.Fragment>
+               <legend>{Resume.heading}</legend>
+               <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    {Tabs}
+               </ul>
+            </React.Fragment>
         );
     }
 }
