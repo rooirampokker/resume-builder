f8d5520dafb217359dcaa40dc1a0781ddcc3a1b7
rooirampokker <rooirampokker@gmail.com>
2019-03-24 22:54:39 +0200

    added capitilization function and encapsulated Utils into class

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
-const Utils = {
+class Utils extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
 
     camelize(str) {
         return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
             return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
         }).replace(/\s+/g, '');
     }
+
+    capitalize(str) {
+        if (typeof str !== 'string')
+            return str;
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
 }
 export default Utils;
