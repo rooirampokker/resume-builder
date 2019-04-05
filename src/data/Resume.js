d20951f6ed1b742ded0ceccc6c3c30302c67cbe7
rooirampokker <rooirampokker@gmail.com>
2019-04-05 12:00:15 +0200

    updated skill

--- src/data/Resume.js
+++ src/data/Resume.js
@@ -9,7 +9,7 @@ const Resume = {
     },
     "Skills Matrix": {
         "Back-End": {
-            "PHP": 4.5,
+            "PHP": 4,
             //"Laravel 2+": 2,
             "SQL": 4,
             "CakePHP": 3,
