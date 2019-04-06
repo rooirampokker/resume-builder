ad5fa4d04ebaefdf2d591356d3ce0f0e87f49149
rooirampokker <rooirampokker@gmail.com>
2019-04-06 12:51:29 +0200

    Added hex-to-rgb converter and method to allow re-cycling of colors in a finite array

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,14 +1,43 @@
-class Utils {
+class Utils {//thank you stack-overflow for this bountiful harvest
+    constructor(props) {
+        this.colorArray  = ["#FF178E",
+            "#9813EB",
+            "#232CFF",
+            "#079DEB"];
+    }
+/*
+*
+ */
     camelize(str) {
         return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
             return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
         }).replace(/\s+/g, '');
     }
-
+/*
+*
+ */
     capitalize(str) {
         if (typeof str !== 'string')
             return str;
         return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+/*
+*
+ */
+    hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+/*
+* Ensures that the available colors are cycled through and re-used properly
+*/
+    pickColor(key) {
+        key = key % this.colorArray.length;
+        return this.colorArray[key];
     }
 
 }
